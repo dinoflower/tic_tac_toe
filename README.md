fbb603703b7c41a8fd0e816f683cb25d9108b7d9
dinoflower <kaedilove@gmail.com>
2022-06-22 17:15:42 -0500

    update readme and note bug

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # tic_tac_toe
-A two-player game of tic-tac-toe, playable in the command line.
+
+A two-player game of tic-tac-toe for the terminal. Made in Ruby.
+
+## Requirements
+
+Requires download of the tic_tac_toe.rb file and Ruby. Playable with
+$ruby tic_tac_toe.rb command. Or, of course, in irb or another environment.
+
+## Development
+
+Initial version of this game used the board to track moves. Non-reusable spaces
+meant that players would be able to tell visually who won.
+
+Current version tracks the positions of plays against a nested array of
+winning moves, as I initially intended.
+
+Due to the simplicity of the program, the "game" and "player" classes are
+stored in a module rather than separate files.
+
+### Notes
+
+Created as part of [The Odin Project](https://www.theodinproject.com).
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,70 +8,88 @@ module TicTacToe
       [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9],
       [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]
     ].freeze
-    attr_accessor :current_player, :winner
-    attr_reader :board, :moves_made
+    attr_accessor :current_player, :winner, :board, :moves_made
 
     def initialize(player1, player2)
-      @board = Array.new(9) { '  ' }
+      @board = Array.new(9) { ' ' }
       @players = [player1.new(self, 'X'), player2.new(self, 'O')]
       @moves_made = {}
-      @winner = 0
+      @winner = ''
       display_board
       @current_player = @players.sample
       puts "#{@current_player.symbol} goes first."
     end
 
+    def display_board
+      row_one = @board[0, 3].join(' | ')
+      row_two = @board[3, 3].join(' | ')
+      row_three = @board[6, 3].join(' | ')
+      bar = '--+---+--'
+      printer_board = "#{row_one}\n#{bar}\n#{row_two}\n#{bar}\n#{row_three}"
+      puts printer_board
+    end
+
     def play_game
-      if @winner != 0
-        puts "Congratulations, #{@winner.symbol}! Play again?"
-      elsif !@board.include?('  ')
-        puts 'We have a tie! Play again?'
-      else
+      while @winner == '' && @board.include?(' ')
         puts "#{@current_player.symbol}, make your move (type a number)."
-        @current_player.place_symbol(gets.chomp.to_i)
+        place_symbol(@current_player)
+      end
+      if @winner != ''
+        puts "Congratulations, #{@winner.symbol}!"
+      elsif !@board.include?(' ')
+        puts 'We have a tie!'
       end
     end
 
-    def display_board
-      row_one = @board[0, 3].join('|')
-      row_two = @board[3, 3].join('|')
-      row_three = @board[6, 3].join('|')
-      bar = '--+--+--'
-      printer_board = "#{row_one}\n#{bar}\n#{row_two}\n#{bar}\n#{row_three}"
-      puts printer_board
+    def place_symbol(player)
+      player.make_play
+      display_board
+      check_winners
+      @current_player = pass_turn
+    end
+
+    def store_data(selection)
+      @board[selection - 1] = @current_player.symbol
+      @moves_made[selection] = @current_player.symbol
+    end
+
+    def check_winners
+      plays = @moves_made.collect { |location, symbol| location if symbol == @current_player.symbol }.compact
+      return unless WIN_CONS.any? { |line| line == line.intersection(plays) }
+
+      @winner = @current_player
+    end
+
+    def pass_turn
+      if @current_player == @players[0]
+        @players[1]
+      else
+        @players[0]
+      end
     end
   end
 
   # each player for the tic-tac-toe game, X and O
   class Player
-    attr_reader :symbol
+    attr_reader :symbol, :game
 
     def initialize(game, symbol)
       @game = game
       @symbol = symbol
     end
 
-    def place_symbol(location)
-      if @board[location - 1] == '  '
-        @board[location - 1] = symbol
-        @moves_made[location] = symbol
+    def make_play
+      selection = gets.chomp.to_i
+      if @game.board[selection - 1] == ' '
+        @game.store_data(selection)
+        # pass_turn should go here
+      elsif @game.board[selection - 1] != ' '
+        puts 'That space is taken! Please choose another.' # currently changes whose turn it is
+      else
+        puts '...'
       end
-      display_board
-      check_winners
-      pass_turn
-    end
-
-    def check_winners
-      plays = @moves_made.collect { |location, symbol| location if symbol == self.symbol }.compact
-      return unless WIN_CONS.any? { |line| line == line.intersection(plays) }
-
-      @winner = self
-    end
-
-    def pass_turn
-      @current_player = !self
     end
   end
 
-  Game.new(Player, Player)
+  Game.new(Player, Player).play_game
 end
