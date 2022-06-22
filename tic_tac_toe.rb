54a03ade19550aa1d5de9e2297c900f8fe8e0261
dinoflower <kaedilove@gmail.com>
2022-06-22 11:18:13 -0500

    adjust styling

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,53 +1,43 @@
-class Game #TicTacToe is too long to write
-  WIN_CONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# frozen_string_literal: true
+
+# one game of tic-tac-toe
+class Game
+  WIN_CONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]].freeze
+  attr_accessor :current_player
   attr_reader :board, :moves_made
-  def initialize
-    @board = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-    @players = [Player.new, PLayer.new]
-    @location = 0
+
+  def initialize(player1, player2)
+    @board = Array.new(9) { @board.fill('_', (0...8)) }
+    @players = [player1.new(self, 'X'), player2.new(self, 'O')]
     @moves_made = {}
     display_board
+    @current_player = @players.sample
+    puts "#{@current_player} goes first."
   end
 
   def display_board
-    @board.each { |row| puts row.to_s}
+    rows = @board.each { |row| row.join('|') }
+    print rows
   end
 end
 
+# each player for the tic-tac-toe game, one for X and one for O
 class Player
   attr_reader :symbol
-  def initialize(symbol)
+
+  def initialize(game, symbol)
+    @game = game
     @symbol = symbol
   end
 
-  def play(player, location)
+  def play(location)
     unless @moves_made.key?(location)
-      @moves_made[location] = player
-      convert_location(player, location)
+      @moves_made[location] = symbol
+      @board[location-1] = symbol
     end
     display_board
   end
-
-  def convert_location(player, location)
-    case location
-    when 1
-      @board[0][0] = player
-    when 2
-      @board[0][1] = player
-    when 3
-      @board[0][2] = player
-    when 4
-      @board[1][0] = player
-    when 5
-      @board[1][1] = player
-    when 6
-      @board[1][2] = player
-    when 7
-      @board[2][0] = player
-    when 8
-      @board[2][1] = player
-    when 9
-      @board[2][2] = player
-    end
-  end
 end
+
+# should something be a module? maybe tictactoe, to group all of these classes together?
+# find win with intersection?
